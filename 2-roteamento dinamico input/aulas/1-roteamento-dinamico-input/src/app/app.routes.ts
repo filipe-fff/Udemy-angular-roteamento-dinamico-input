8428b67073f7d0a979a25793f6cd5de32f860275
filipe-fff <pintovicentinifilipe@gmail.com>
2024-12-22 19:51:44 -0300

    criação da rota com parametro dinamico

--- 2-roteamento dinamico input/aulas/1-roteamento-dinamico-input/src/app/app.routes.ts
+++ 2-roteamento dinamico input/aulas/1-roteamento-dinamico-input/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { UsersComponent } from './components/users/users.component';
+import { PostsComponent } from './components/posts/posts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        pathMatch: "full",
+        redirectTo: "users"
+    },
+    {
+        path: "users",
+        component: UsersComponent
+    },
+    {
+        path: "posts/:userId",
+        component: PostsComponent
+    }
+];

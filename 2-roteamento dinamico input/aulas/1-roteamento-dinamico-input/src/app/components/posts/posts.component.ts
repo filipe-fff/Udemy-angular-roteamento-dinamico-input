26006d763d17081170d29c5d252590ffc8d15aa4
filipe-fff <pintovicentinifilipe@gmail.com>
2024-12-22 20:53:45 -0300

    acessando o parametro estatico da url com inputi

--- 2-roteamento dinamico input/aulas/1-roteamento-dinamico-input/src/app/components/posts/posts.component.ts
+++ 2-roteamento dinamico input/aulas/1-roteamento-dinamico-input/src/app/components/posts/posts.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { UsersPostList } from '../../types/users-post-list';
+import { UserPost } from '../../services/user-post.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [],
+  imports: [ AsyncPipe ],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss'
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
+  @Input() userId: string = "";
 
+  postsList$: Observable<UsersPostList> = of([]);
+  private readonly _postsList = inject(UserPost);
+
+  ngOnInit() {
+      console.log(this.userId);
+      this.postsList$ = this._postsList.getUserPost(this.userId);
+  }
 }

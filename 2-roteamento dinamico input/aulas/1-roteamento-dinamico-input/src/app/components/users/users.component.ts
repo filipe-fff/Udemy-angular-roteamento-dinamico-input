fe79056206be5319f8b5354c55480e345aeae8e8
filipe-fff <pintovicentinifilipe@gmail.com>
2024-12-22 20:21:43 -0300

    passando o routerLink com parametros

--- 2-roteamento dinamico input/aulas/1-roteamento-dinamico-input/src/app/components/users/users.component.ts
+++ 2-roteamento dinamico input/aulas/1-roteamento-dinamico-input/src/app/components/users/users.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { UsersList } from '../../types/users-list';
+import { UsersService } from '../../services/users.service';
+import { AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [ AsyncPipe, RouterLink ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  usersList$: Observable<UsersList> = of([]);
 
+  private readonly _usersList = inject(UsersService);
+
+  ngOnInit(): void {
+      this.usersList$ = this._usersList.getUsers();
+  }
 }

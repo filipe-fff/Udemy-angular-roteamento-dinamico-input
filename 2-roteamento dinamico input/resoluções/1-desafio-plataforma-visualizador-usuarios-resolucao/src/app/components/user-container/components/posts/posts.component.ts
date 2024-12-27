dba21560dfb714ccd6caefb1f399f47efc66a80c
filipe-fff <pintovicentinifilipe@gmail.com>
2024-12-27 20:32:03 -0300

    Carregamento da lista de Posts do Usuário

--- 2-roteamento dinamico input/resoluções/1-desafio-plataforma-visualizador-usuarios-resolucao/src/app/components/user-container/components/posts/posts.component.ts
+++ 2-roteamento dinamico input/resoluções/1-desafio-plataforma-visualizador-usuarios-resolucao/src/app/components/user-container/components/posts/posts.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PostsListService } from '../../../../services/posts-list.service';
+import { Observable, of } from 'rxjs';
+import { PostsListResponse } from '../../../../types/posts-list-response';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe
+  ],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss'
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
+  postsList$: Observable<PostsListResponse> = of([]);
 
+  private readonly _activatedRoute = inject(ActivatedRoute);
+  private readonly _postsListService = inject(PostsListService);
+
+  ngOnInit() {
+      this._activatedRoute.parent?.params.subscribe(
+        (params) => this.postsList$ = this._postsListService.getUserPosts(params["userId"])
+      );
+  }
 }

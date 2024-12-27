c0fdf990864d52c2ad8ede5aa325384f2bad9949
filipe-fff <pintovicentinifilipe@gmail.com>
2024-12-27 13:42:47 -0300

    criação da rota de usuários da resolução

--- 2-roteamento dinamico input/resoluções/1-desafio-plataforma-visualizador-usuarios-resolucao/src/app/app.routes.ts
+++ 2-roteamento dinamico input/resoluções/1-desafio-plataforma-visualizador-usuarios-resolucao/src/app/app.routes.ts
@@ -1,7 +1,18 @@
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
 
 export const routes: Routes = [
+    {
+        path: "",
+        pathMatch: "full",
+        redirectTo: "users-list"
+    },
+    {
+        path: "users-list",
+        title: "Lista de usuários",
+        component: UsersListComponent
+    },
     {
         path: "**",
         title: "Página não Encontrada",
---
+++ 2-roteamento dinamico input/resoluções/1-desafio-plataforma-visualizador-usuarios-resolucao/src/app/components/users-list/users-list.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { UsersListResponse } from '../../types/users-list-response';
+import { UsersListService } from '../../services/users-list.service';
+import { Observable, of } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-users-list',
+  standalone: true,
+  imports: [
+    AsyncPipe
+  ],
+  templateUrl: './users-list.component.html',
+  styleUrl: './users-list.component.scss'
+})
+export class UsersListComponent implements OnInit {
+  private readonly _usersListService = inject(UsersListService);
+  usersList$: Observable<UsersListResponse> = of([]);
+
+  ngOnInit() {
+      this.usersList$ = this._usersListService.getUsers();
+  }
+}

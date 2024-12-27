f586087caa7665d511fdb4a2db964a6a549a01cd
filipe-fff <pintovicentinifilipe@gmail.com>
2024-12-27 17:55:40 -0300

    recuperando os dados do usuário selecionado da resolução do desafio

--- 2-roteamento dinamico input/resoluções/1-desafio-plataforma-visualizador-usuarios-resolucao/src/app/components/user-container/user-container.component.ts
+++ 2-roteamento dinamico input/resoluções/1-desafio-plataforma-visualizador-usuarios-resolucao/src/app/components/user-container/user-container.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { IUser } from '../../interfaces/user/user.interface';
+import { UsersListService } from '../../services/users-list.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-user-container',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe
+  ],
   templateUrl: './user-container.component.html',
   styleUrl: './user-container.component.scss'
 })
 export class UserContainerComponent {
+  user$: Observable<IUser> = of({} as IUser);
 
+  private readonly _usersListService = inject(UsersListService);
+
+  @Input() set userId(value: string) {
+    this.user$ = this._usersListService.getUser(value);
+  };
 }

090849e3d37b503361debf1c3021cb6f60df2a5b
filipe-fff <pintovicentinifilipe@gmail.com>
2024-12-28 03:56:49 -0300

    Carregamento da lista de comentários do post

--- 2-roteamento dinamico input/resoluções/1-desafio-plataforma-visualizador-usuarios-resolucao/src/app/components/user-container/components/post/components/comments/comments.component.ts
+++ 2-roteamento dinamico input/resoluções/1-desafio-plataforma-visualizador-usuarios-resolucao/src/app/components/user-container/components/post/components/comments/comments.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommentsListService } from '../../../../../../services/comments-list.service';
+import { Observable, of } from 'rxjs';
+import { CommentsListResponse } from '../../../../../../types/comments-list-response';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-comments',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe
+  ],
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.scss'
 })
-export class CommentsComponent {
+export class CommentsComponent implements OnInit {
+  commentsList$: Observable<CommentsListResponse> = of([]);
 
+  private readonly _activatedRoute = inject(ActivatedRoute);
+  private readonly _commentsListService = inject(CommentsListService);
+
+  ngOnInit() {
+      this._activatedRoute.parent?.params.subscribe(
+        (params) => this.commentsList$ = this._commentsListService.getPostComments(params["postId"])
+      );
+  }
 }

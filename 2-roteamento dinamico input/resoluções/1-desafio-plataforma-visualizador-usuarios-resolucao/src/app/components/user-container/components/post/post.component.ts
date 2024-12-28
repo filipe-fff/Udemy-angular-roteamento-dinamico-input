98883a4509929b025963726bfff512080f101ef3
filipe-fff <pintovicentinifilipe@gmail.com>
2024-12-28 00:33:12 -0300

    carregamento da lista de comentários do do post

--- 2-roteamento dinamico input/resoluções/1-desafio-plataforma-visualizador-usuarios-resolucao/src/app/components/user-container/components/post/post.component.ts
+++ 2-roteamento dinamico input/resoluções/1-desafio-plataforma-visualizador-usuarios-resolucao/src/app/components/user-container/components/post/post.component.ts
@@ -3,14 +3,15 @@ import { PostsListService } from '../../../../services/posts-list.service';
 import { Observable, of } from 'rxjs';
 import { IPost } from '../../../../interfaces/post.interface';
 import { AsyncPipe } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-post',
   standalone: true,
   imports: [
     AsyncPipe,
-    RouterLink
+    RouterLink,
+    RouterOutlet
   ],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'

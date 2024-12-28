f7379a1e71ff2a2776f2aaf9dcfdbfbd532feb8a
filipe-fff <pintovicentinifilipe@gmail.com>
2024-12-28 20:29:43 -0300

    Acessando Query Strings Dinamicos com o @Input e ActivatedRoute

--- 2-roteamento dinamico input/aulas/2-query-strings-e-data/src/app/components/comp1/comp1.component.ts
+++ 2-roteamento dinamico input/aulas/2-query-strings-e-data/src/app/components/comp1/comp1.component.ts
@@ -9,15 +9,20 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './comp1.component.scss'
 })
 export class Comp1Component implements OnInit {
-  @Input() name: string = "";
+  // @Input() name: string = "";
+  @Input() set name(value: string) {
+    console.log("value", value);
+  }
 
   private readonly _activatedRoute = inject(ActivatedRoute);
 
   ngOnInit() {
       console.log("ngOnInit Comp1Component");
 
-      console.log("name", this.name);
+      // console.log("name", this.name);
 
       console.log("snapshot => ", this._activatedRoute.snapshot.queryParams);
+
+      this._activatedRoute.queryParams.subscribe(params => console.log("Query Params", params));
   }
 }
